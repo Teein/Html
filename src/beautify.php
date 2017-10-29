8b1abec7de235fdc5baf8ffbc73557f0e0a0a51d
Raoul Schaffranek <raoul.schaffranek@rwth-aachen.de>
2017-10-29 15:57:28 +0100

    [Bugfix] beautify now returns an object of type Beautify

--- src/beautify.php
+++ src/beautify.php
@@ -5,7 +5,7 @@ namespace Teein\Html\Beautifier;
 
 use Teein\Html\VirtualDom\Document;
 
-function beautify(Document $document) : Document
+function beautify(Document $document) : Beautifier
 {
     return $document->beautify();
 }
